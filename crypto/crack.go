bff8811aa6d46d7ccec95c12a26de26bec327e91
Simcha Berkovitz <151738331+Simcha-b@users.noreply.github.com>
2025-06-12 18:12:47 +0300

    Add Caesar cipher implementation with encryption and decryption options

---
+++ crypto/crack.go
@@ -0,0 +1,13 @@
+// package main
+
+// // return the  Frequency analysis
+
+
+// func frequencyAnalysis(text string) map[rune]float64 {
+
+// 	mp := make(map[rune]int)
+
+// 	for i := range text{
+		
+// 	}
+// }
--- crypto/main.go
+++ crypto/main.go
@@ -3,46 +3,71 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
+	"math/rand"
 	"os"
+	"sync"
+
+	"github.com/common-nighthawk/go-figure"
 )
 
 func main() {
-	fmt.Println("Welcome to the ENIGMA!!!!")
-	moveNum := 3 
-	kayMap := createKayMap(moveNum)
-	fmt.Println("Chose what you want to do:")
-	fmt.Println("1. Encrypt text")
-	fmt.Println("2. Decrypt text")
-	var choice int
-	fmt.Scanf("%d", &choice)
-	bufio.NewReader(os.Stdin).ReadString('\n')
-	switch choice {
-	case 1:
-		fmt.Println("You chose to encrypt text")
-		// text := readFromUser()
-		text := readFromFile()
-		if text == "" {
-			fmt.Println("No text found in input.txt, please provide input.")
+	myFigure := figure.NewColorFigure("WELCOME TO THE ENIGMA!", "","red", true)
+	myFigure.Print()
+	fmt.Println("This is a simple Caesar cipher implementation.")
+	fmt.Println("You can encrypt and decrypt text using a key.")
+	for {
+		fmt.Println("Chose what you want to do:")
+		fmt.Println("1. Encrypt text")
+		fmt.Println("2. Decrypt text with a key")
+		fmt.Println("3. Decrypt text without a key")
+		var choice int
+		fmt.Scanf("%d", &choice)
+		bufio.NewReader(os.Stdin).ReadString('\n')
+		switch choice {
+		case 1:
+			fmt.Println("You chose to encrypt text")
+			// text := readFromUser()
+			text := readFromFile()
+			if text == "" {
+				fmt.Println("No text found in input.txt, please provide input.")
+				return
+			}
+			encryptedText, nomMove := encryptText(text)
+			writeToFile(encryptedText)
+			fmt.Printf("Encrypted text with key: %d\n", nomMove)
+			fmt.Println("Encrypted text is written to output.txt")
+			fmt.Println("Encrypt successful!!")
+		case 2:
+			fmt.Println("You chose to decrypt text")
+			fmt.Println("Please enter the key (number of positions to shift): ")
+			// Read the key from user input
+			var kay int
+			fmt.Scanf("%d", &kay)
+			bufio.NewReader(os.Stdin).ReadString('\n')
+			text := readFromFile()
+			if text == "" {
+				fmt.Println("No text found in input.txt, please provide input.")
+				return
+			}
+			if kay < 1 || kay > 25 {
+				fmt.Println("Invalid key, please enter a number between 1 and 25")
+				return
+			}
+			// Create the mapping based on the key
+			kayMap := createKayMap(kay)
+			// Decrypt the text using the same mapping
+			decryptedText := decryptText(text, kayMap)
+			writeToFile(decryptedText)
+			fmt.Println("Decrypted successful!!")
+
+		case 3:
+			decryptTextWithoutKay(readFromFile())
+			fmt.Println("Decrypted successful!!, check the results")
+		default:
+			fmt.Println("Invalid choice, please choose 1 or 2")
 			return
 		}
-		encryptedText := encryptText(text, kayMap)
-		writeToFile(encryptedText)
-		fmt.Println("Encrypt successful!!")
-	case 2:
-		fmt.Println("You chose to decrypt text")
-		// text := readFromUser()
-		text := readFromFile()
-		if text == "" {
-			fmt.Println("No text found in input.txt, please provide input.")
-			return
-		}
-		// Decrypt the text using the same mapping
-		decryptedText := decryptText(text, kayMap)
-		writeToFile(decryptedText)
-		fmt.Println("Decrypted successful!!")
-	default:
-		fmt.Println("Invalid choice, please choose 1 or 2")
-		return
 	}
 }
 
@@ -72,7 +97,10 @@ func createKayMap(moveNum int) map[rune]rune {
 }
 
 // encryptText encrypts the input text using the provided mapping
-func encryptText(text string, kayMap map[rune]rune) string {
+func encryptText(text string) (string, int) {
+	numMove := rand.Intn(26) + 1
+	fmt.Printf("Num: %d\n", numMove)
+	kayMap := createKayMap(numMove)
 	encrypted := []rune{}
 	for _, char := range text {
 		if val, ok := kayMap[char]; ok {
@@ -81,7 +109,7 @@ func encryptText(text string, kayMap map[rune]rune) string {
 			encrypted = append(encrypted, char)
 		}
 	}
-	return string(encrypted)
+	return string(encrypted), numMove
 }
 
 // decryptText decrypts the input text using the provided mapping
@@ -125,20 +153,50 @@ func readFromFile() string {
 
 // writeToFile writes the given text to output.txt
 func writeToFile(text string) {
-	file, err := os.Create("output.txt")
+	file, err := os.OpenFile("output.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		fmt.Println("Error creating file:", err)
+		fmt.Println("Error opening file:", err)
 		return
 	}
 	defer file.Close()
+
 	writer := bufio.NewWriter(file)
-	_, err = writer.WriteString(text)
+
+	_, err = writer.WriteString(text + "\n")
 	if err != nil {
 		fmt.Println("Error writing to file:", err)
 		return
 	}
+
 	writer.Flush()
-	fmt.Println("Output written to output.txt")
+	fmt.Println("Text appended to output.txt")
+}
+
+func decryptTextWithoutKay(text string) {
+
+	wg := sync.WaitGroup{}
+
+	mt := sync.Mutex{}
+
+	for i := 1; i < 26; i++ {
+		wg.Add(1)
+
+		go func(i int) {
+
+			defer wg.Done()
+
+			tempText := decryptText(text, createKayMap(i))
+
+			mt.Lock()
+			writeToFile(fmt.Sprintf("\n==== Try #%d ====\nShift: %d\nDecrypted Text:\n%s\n====================", i, i, tempText))
+			mt.Unlock()
+
+			log.Printf("Try #%d with shift %d:\n", i, i)
+			fmt.Println(tempText)
+		}(i)
+
+	}
+	wg.Wait()
 }
 
 
